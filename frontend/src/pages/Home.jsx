c931379c07609a1c18fe8ffd450e4c2b67a732ea
Imtinan <imtinanfakhar879@gmail.com>
2024-04-08 17:29:47 +0500

    Add files via upload

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Hero from '../components/Hero'
+import Biography from '../components/Biography'
+import Departments from '../components/Departments'
+import MessageForm from '../components/MessageForm'   
+
+const Home = () => {
+  return (
+    <>
+        <Hero title={"Welcome to ZeeCare Medical Institute | Your Trusted HealthCare Provider"} imageUrl={"/hero.png"}/>
+        <Biography imageUrl={"/about.png"}/>
+        <Departments/>
+        <MessageForm/>
+    </> 
+  )
+}
+
+export default Home
